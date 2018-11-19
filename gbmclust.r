0a94e43e63d7eafc3421875b3cee55ce6c36077b
Luke Hayden <luke@portailwifi.ens-lyon.fr>
2018-11-19 15:21:59 +0100

    models for cluster

--- gbmclust.r
+++ gbmclust.r
@@ -29,82 +29,6 @@ md <- as.data.frame(subset(t(ctall.norm), sampleinfo$sex == "F" &
 
 md <- subset(md, rownames(md)%in% qualsum$sample[qualsum$good])
 
-md <- md[, colnames(md) %in% c(minf$name[minf$in61==T], "predage")]
-
-md$sample <- rownames(md)
-
-md <- left_join(md, select(sampleinfo, sample, predage), by="sample") %>%
-  filter(!(is.na(predage))) %>%
-  column_to_rownames(var="sample")
-
-
-trainchoice <- sample(1:nrow(md))[1:floor(4*(nrow(md)/5))]
-
-
-trainchoice <- rownames(md) %in% rownames(currbest$trainingData)
-trdat <- md[trainchoice,]
-tedat <- md[-trainchoice,]
-
-
-gbmagemodel1 <- train(
-  predage~.,
-  tuneLength = 100,
-  data = trdat, 
-  method = "gbm",
-  tuneGrid=expand.grid(n.trees = (0:50)*200, 
-                       interaction.depth = c(1,2,3,4,5,6,7,8,9,10), 
-                       shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
-                       n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
-  trControl =  trainControl(method = 'cv', 
-                            number = 50, 
-                            summaryFunction=defaultSummary, 
-                            verboseIter = T)
-)
-
-
-gbmagemodel2 <- train(
-  predage~.,
-  tuneLength = 10,
-  data = trdat, 
-  method = "gbm",
-  tuneGrid=expand.grid(n.trees = (0:50)*200, 
-                       interaction.depth = c(1,2,3,4,5,6,7,8,9,10), 
-                       shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
-                       n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
-  trControl =  trainControl(method = 'cv', 
-                            number = 50, 
-                            summaryFunction=defaultSummary, 
-                            verboseIter = T)
-)
-
-
-gbmagemodel3 <- train(
-  predage~.,
-  tuneLength = 10,
-  data = trdat, 
-  method = "gbm",
-  tuneGrid=expand.grid(n.trees = (0:50)*200, 
-                       interaction.depth = c(1,2,3,4,5,6,7,8,9,10), 
-                       shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
-                       n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
-  trControl =  trainControl(method = 'cv', 
-                            number = 20, 
-                            summaryFunction=defaultSummary, 
-                            verboseIter = T)
-)
-
-
-
-
-md <- as.data.frame(subset(t(ctall.norm), sampleinfo$sex == "F" &
-                             sampleinfo$prep== "Luke"&
-                             sampleinfo$exp %in% c("Size-age", "cohorts", "OvY") &
-                             sampleinfo$codeset == "phaw_1" &
-                             sampleinfo$type %in% c("O", "Y", "M")
-))
-
-md <- subset(md, rownames(md)%in% qualsum$sample[qualsum$good])
-
 md <- md[, lmdf2$in61 ==T & lmdf2$pvalovy < 0.05]
 
 md <- md[, colnames(md) %in% c(minf$name[minf$in61==T], "predage")]
@@ -124,9 +48,9 @@ trdat <- md[trainchoice,]
 tedat <- md[-trainchoice,]
 
 
-gbmagemodel4 <- train(
+gbmagemodelp5 <- train(
   predage~.,
-  tuneLength = 100,
+  tuneLength = 5,
   data = trdat, 
   method = "gbm",
   tuneGrid=expand.grid(n.trees = (0:50)*200, 
@@ -134,15 +58,42 @@ gbmagemodel4 <- train(
                        shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
                        n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
   trControl =  trainControl(method = 'cv', 
-                            number = 50, 
+                            number = 10, 
                             summaryFunction=defaultSummary, 
                             verboseIter = T)
 )
 
+md <- as.data.frame(subset(t(ctall.norm), sampleinfo$sex == "F" &
+                             sampleinfo$prep== "Luke"&
+                             sampleinfo$exp %in% c("Size-age", "cohorts", "OvY") &
+                             sampleinfo$codeset == "phaw_1" &
+                             sampleinfo$type %in% c("O", "Y", "M")
+))
 
-gbmagemodel5 <- train(
+md <- subset(md, rownames(md)%in% qualsum$sample[qualsum$good])
+
+md <- md[, lmdf2$in61 ==T & lmdf2$pvalovy < 0.04]
+
+md <- md[, colnames(md) %in% c(minf$name[minf$in61==T], "predage")]
+
+md$sample <- rownames(md)
+
+md <- left_join(md, select(sampleinfo, sample, predage), by="sample") %>%
+  filter(!(is.na(predage))) %>%
+  column_to_rownames(var="sample")
+
+
+trainchoice <- sample(1:nrow(md))[1:floor(4*(nrow(md)/5))]
+
+
+trainchoice <- rownames(md) %in% rownames(currbest$trainingData)
+trdat <- md[trainchoice,]
+tedat <- md[-trainchoice,]
+
+
+gbmagemodelp4 <- train(
   predage~.,
-  tuneLength = 10,
+  tuneLength = 5,
   data = trdat, 
   method = "gbm",
   tuneGrid=expand.grid(n.trees = (0:50)*200, 
@@ -150,18 +101,162 @@ gbmagemodel5 <- train(
                        shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
                        n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
   trControl =  trainControl(method = 'cv', 
-                            number = 50, 
+                            number = 10, 
                             summaryFunction=defaultSummary, 
                             verboseIter = T)
 )
 
 
-gbmagemodel6 <- train(
+md <- as.data.frame(subset(t(ctall.norm), sampleinfo$sex == "F" &
+                             sampleinfo$prep== "Luke"&
+                             sampleinfo$exp %in% c("Size-age", "cohorts", "OvY") &
+                             sampleinfo$codeset == "phaw_1" &
+                             sampleinfo$type %in% c("O", "Y", "M")
+))
+
+md <- subset(md, rownames(md)%in% qualsum$sample[qualsum$good])
+
+md <- md[, lmdf2$in61 ==T & lmdf2$pvalovy < 0.03]
+
+md <- md[, colnames(md) %in% c(minf$name[minf$in61==T], "predage")]
+
+md$sample <- rownames(md)
+
+md <- left_join(md, select(sampleinfo, sample, predage), by="sample") %>%
+  filter(!(is.na(predage))) %>%
+  column_to_rownames(var="sample")
+
+
+trainchoice <- sample(1:nrow(md))[1:floor(4*(nrow(md)/5))]
+
+
+trainchoice <- rownames(md) %in% rownames(currbest$trainingData)
+trdat <- md[trainchoice,]
+tedat <- md[-trainchoice,]
+
+
+gbmagemodelp3 <- train(
   predage~.,
-  tuneLength = 10,
+  tuneLength = 5,
   data = trdat, 
   method = "gbm",
   tuneGrid=expand.grid(n.trees = (0:50)*200, 
+                       interaction.depth = c(1,2,3,4,5,6,7,8,9,10), 
+                       shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
+                       n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
+  trControl =  trainControl(method = 'cv', 
+                            number = 10, 
+                            summaryFunction=defaultSummary, 
+                            verboseIter = T)
+)
+
+md <- as.data.frame(subset(t(ctall.norm), sampleinfo$sex == "F" &
+                             sampleinfo$prep== "Luke"&
+                             sampleinfo$exp %in% c("Size-age", "cohorts", "OvY") &
+                             sampleinfo$codeset == "phaw_1" &
+                             sampleinfo$type %in% c("O", "Y", "M")
+))
+
+md <- subset(md, rownames(md)%in% qualsum$sample[qualsum$good])
+
+md <- md[, lmdf2$in61 ==T & lmdf2$pvalovy < 0.02]
+
+md <- md[, colnames(md) %in% c(minf$name[minf$in61==T], "predage")]
+
+md$sample <- rownames(md)
+
+md <- left_join(md, select(sampleinfo, sample, predage), by="sample") %>%
+  filter(!(is.na(predage))) %>%
+  column_to_rownames(var="sample")
+
+
+trainchoice <- sample(1:nrow(md))[1:floor(4*(nrow(md)/5))]
+
+
+trainchoice <- rownames(md) %in% rownames(currbest$trainingData)
+trdat <- md[trainchoice,]
+tedat <- md[-trainchoice,]
+
+
+gbmagemodelp2 <- train(
+  predage~.,
+  tuneLength = 5,
+  data = trdat, 
+  method = "gbm",
+  tuneGrid=expand.grid(n.trees = (0:50)*200, 
+                       interaction.depth = c(1,2,3,4,5,6,7,8,9,10), 
+                       shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
+                       n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
+  trControl =  trainControl(method = 'cv', 
+                            number = 10, 
+                            summaryFunction=defaultSummary, 
+                            verboseIter = T)
+)
+
+md <- as.data.frame(subset(t(ctall.norm), sampleinfo$sex == "F" &
+                             sampleinfo$prep== "Luke"&
+                             sampleinfo$exp %in% c("Size-age", "cohorts", "OvY") &
+                             sampleinfo$codeset == "phaw_1" &
+                             sampleinfo$type %in% c("O", "Y", "M")
+))
+
+md <- subset(md, rownames(md)%in% qualsum$sample[qualsum$good])
+
+md <- md[, lmdf2$in61 ==T & lmdf2$pvalovy < 0.01]
+
+md <- md[, colnames(md) %in% c(minf$name[minf$in61==T], "predage")]
+
+md$sample <- rownames(md)
+
+md <- left_join(md, select(sampleinfo, sample, predage), by="sample") %>%
+  filter(!(is.na(predage))) %>%
+  column_to_rownames(var="sample")
+
+
+trainchoice <- sample(1:nrow(md))[1:floor(4*(nrow(md)/5))]
+
+
+trainchoice <- rownames(md) %in% rownames(currbest$trainingData)
+trdat <- md[trainchoice,]
+tedat <- md[-trainchoice,]
+
+
+gbmagemodelp1 <- train(
+  predage~.,
+  tuneLength = 5,
+  data = trdat, 
+  method = "gbm",
+  tuneGrid=expand.grid(n.trees = (0:50)*200, 
+                       interaction.depth = c(1,2,3,4,5,6,7,8,9,10), 
+                       shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
+                       n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
+  trControl =  trainControl(method = 'cv', 
+                            number = 10, 
+                            summaryFunction=defaultSummary, 
+                            verboseIter = T)
+)
+
+gbmagemodelp1b <- train(
+  predage~.,
+  tuneLength = 5,
+  data = trdat, 
+  method = "gbm",
+  tuneGrid=expand.grid(n.trees = (0:50)*500, 
+                       interaction.depth = c(1,2,3,4,5,6,7,8,9,10), 
+                       shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
+                       n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
+  trControl =  trainControl(method = 'cv', 
+                            number = 10, 
+                            summaryFunction=defaultSummary, 
+                            verboseIter = T)
+)
+
+gbmagemodelp1c <- train(
+  predage~.,
+  tuneLength = 5,
+  data = trdat, 
+  method = "gbm",
+  tuneGrid=expand.grid(n.trees = (0:50)*500, 
                        interaction.depth = c(1,2,3,4,5,6,7,8,9,10), 
                        shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
                        n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
@@ -171,23 +266,21 @@ gbmagemodel6 <- train(
                             verboseIter = T)
 )
 
-
-gbmagemodel4 <- train(
+gbmagemodelp1d <- train(
   predage~.,
-  tuneLength = 4,
+  tuneLength = 5,
   data = trdat, 
   method = "gbm",
-  tuneGrid=expand.grid(n.trees = (0:50)*200, 
+  tuneGrid=expand.grid(n.trees = (0:50)*500, 
                        interaction.depth = c(1,2,3,4,5,6,7,8,9,10), 
                        shrinkage = c(0.0001,.001, 0.01, 0.1, 0.2, 0.3) ,
                        n.minobsinnode = c(2,3,4,5,6,7,8,9,10)),
   trControl =  trainControl(method = 'cv', 
-                            number = 50, 
+                            number = 40, 
                             summaryFunction=defaultSummary, 
                             verboseIter = T)
 )
 
-
 stopCluster(cl)
 
-save(gbmagemodel1,gbmagemodel2, gbmagemodel3, gbmagemodel4, gbmagemodel5, gbmagemodel6, gbmagemodel7,  file="gbmbig.rdata")
+save(gbmagemodelp5, gbmagemodelp4, gbmagemodelp3, gbmagemodelp2, gbmagemodelp1, gbmagemodelp1b, gbmagemodelp1c, gbmagemodelp1d, file="gbmbig2.rdata")
