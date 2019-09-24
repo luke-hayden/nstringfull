289a4515b31c711d2e07e6bfec2ca7f20a2f2a54
Luke <luke.hayden@ens=lyon.fr>
2019-09-25 00:11:39 +0200

    new data

---
+++ imp2.R
@@ -0,0 +1,25 @@
+
+
+library(NanoStringNorm)
+
+
+dirstart <-"~/Documents/nstringfull/"
+
+
+setwd(paste0(dirstart, "1617"))
+ct1617.raw <- read.markup.RCC()
+
+
+setwd(paste0(dirstart, "1819"))
+ct1819.raw <- read.markup.RCC()
+
+
+
+setwd(paste0(dirstart, "2021"))
+ct2021.raw <- read.markup.RCC()
+
+
+
+setwd(paste0(dirstart, "2223"))
+ct2223.raw <- read.markup.RCC()
+
